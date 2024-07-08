f54c8c836be68dcaef4fba6dccf9066b38a78627
Blue <119905639+2MuchBlue@users.noreply.github.com>
2024-07-07 20:10:03 -0500

    Created basics.js
    Added basics.js to help give reference and to use for making games easier

---
+++ basics.js
@@ -0,0 +1,95 @@
+// ===== Canvas Constants =====
+const canvasElement = document.getElementById('mainCanvas');
+const ctx = canvasElement.getContext('2d');
+const canvasHalfWidth = canvasElement.width * 0.5;
+const canvasHalfHeight = canvasElement.height * 0.5;
+
+const deg2rad = Math.PI / 180;
+
+// ===== Input =====
+    canvasElement.addEventListener('mousemove', function(e){
+        mouse.x = (e.offsetX / canvasElement.clientWidth) * canvasElement.width;
+        mouse.y = (e.offsetY / canvasElement.clientHeight) * canvasElement.height;
+
+        mouse.movementX = e.movementX;
+        mouse.movementY = e.movementY;
+    });
+
+    document.addEventListener('mousedown', function(e){
+        mouse[e.button] = true;
+    });
+    
+    document.addEventListener('mouseup', function(e){
+        mouse[e.button] = false;
+    });
+
+    let keys = {};
+    let mouse = {
+        0: false,
+        1: false,
+        2: false,
+
+        real : {
+          x: 0,
+          y: 0,
+        },
+
+        get x() {
+          return Math.floor(mouse.real.x);
+        },
+
+        get y() {
+          return Math.floor(mouse.real.y);
+        },
+
+        movementX : 0,
+        movementY : 0,
+
+        inAreaCheck(x, y, w, h){
+            return (mouse.x >= x && mouse.x <= x + w && mouse.y >= y && mouse.y <= y + h);
+        }
+    };
+
+    let Time = {
+        deltaTime : 0,
+        now : 0,
+        launchTime : new Date().getTime(),
+        startGameTime: 0,
+        frameCount : 0,
+        engineCount : 0,
+    };    
+
+    document.addEventListener('keydown', function(e){
+        keys[e.code] = true;
+    });
+
+    document.addEventListener('keyup', function(e){
+        keys[e.code] = false;
+    });
+
+    function btn(button){
+        return (keys[button] === true ? 1 : 0);
+    }
+
+    let now = new Date();
+    now = now.getTime();
+    let lastTime = now;
+
+    function engineUpdate(){
+        let now = new Date();
+        now = now.getTime();
+        Time.now = now;
+
+        Time.deltaTime = now - lastTime;
+        lastTime = now;
+
+        if(Time.deltaTime < 100){
+            gameUpdate();
+            Time.frameCount++;
+        }else{
+            console.log("You've Left!");
+        }
+
+        Time.engineCount++;
+        requestAnimationFrame(engineUpdate);
+    }
