654a738ec59b3cef95ee25b5697bc37e06673f5a
Blue <119905639+2MuchBlue@users.noreply.github.com>
2024-07-23 19:18:56 -0500

    normalize vector function fixted for real

--- Math2.js
+++ Math2.js
@@ -38,6 +38,7 @@ const Math2 = {
     },
     
     normalizeVector(vec){
+	if(vec === new vec2(0, 0)) { return new vec2(0, 0); }
         let dist = Math2.distance(new vec2(0, 0), vec);
         vec.x = vec.x / dist;
         vec.y = vec.y / dist;
@@ -51,7 +52,7 @@ const Math2 = {
         let normalPoint = new vec2(x, y);
     
         let dist = Math2.distance(new vec2(0, 0), normalPoint);
-	if(dist === 0){ return new vec2(0, 0); }
+	
         normalPoint.x = normalPoint.x / dist;
         normalPoint.y = normalPoint.y / dist;
 		
